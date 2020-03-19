2dc901188fbd7afcc5fcd1404cccc63cb123e8f2
Elf-mousE <ifmouz@gmail.com>
2020-03-19 09:26:55 +0800

    update balm@2.10.0

--- templates/config/helpers.js
+++ templates/config/helpers.js
@@ -1,10 +1,9 @@
+/* eslint-env node */
 const path = require('path');
 
 const _root = path.resolve(__dirname, '..');
 
-function root(args) {
+exports.root = function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [_root].concat(args));
-}
-
-exports.root = root;
+};
--- templates/config/publish.js
+++ templates/config/publish.js
@@ -1,4 +1,5 @@
-module.exports = [
+/* eslint-env node */
+const templates = [
   {
     input: 'index.html',
     output: 'views', // Replace 'views' to your remote project views path
@@ -8,3 +9,12 @@ module.exports = [
     }
   }
 ];
+
+module.exports = function publish(mix) {
+  if (mix.env.isProd) {
+    // Publish assets
+    mix.publish();
+    // Publish templates
+    mix.publish(templates);
+  }
+};
